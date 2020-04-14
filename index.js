198105fe2b29a26874981b320239676c5de6cb54
Izaak Chukwuma <46270445+izaakwalz@users.noreply.github.com>
2020-04-14 03:53:41 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let mongoClient = require('mongodb').MongoClient;
-
-let dbUrl = 'mongodb://localhost:27017/walz';
-mongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, db) => {
-  if (err) throw err;
-  console.log('Database created by walz');
-  db.close();
-});
-// { useUnifiedTopology: true } given error
